858a813f1db891056cd744fff2f2508e8a267b15
omkashyap007 <omkashyapcric@gmail.com>
2023-03-24 21:08:27 +0530

    initial Commit

--- base/views.py
+++ base/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import RPi.GPIO as gp
 import time
-gp.setwarnings(False)
-gp.setmode(gp.BOARD)
-gp.setup(5 , gp.OUT)
-    
+# gp.setwarnings(False)
+# gp.setmode(gp.BOARD)
+# gp.setup(5 , gp.OUT)
+# gp.setup(3 , gp.IN)
+# val = gp.input(3)
+print(val)
 def homePage(request , *args , **kwargs) :
     command = int(request.GET.get("command"))
     if command == 1 :
