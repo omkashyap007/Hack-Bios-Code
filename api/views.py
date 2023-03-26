062d28e0f65d9de5c4960ee2f4683c81c37d1bed
omkashyap007 <omkashyapcric@gmail.com>
2023-03-26 23:30:13 +0530

    Added Multi Interface Access

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .gpio_functions import *
+from .async_functions import *
 
 @api_view(["POST"])
 def changeDeviceState(request , *args ,**kwargs):
@@ -11,13 +12,14 @@ def changeDeviceState(request , *args ,**kwargs):
         "errors" : None , 
         "success" : False , 
     }
-    button_number = request.POST.get("button_number")
-    state_change_value = request.POST.get("state_chanage_value")
-    
-    state_changed = toggleDevice(switch , relay)
+    button_number = int(request.POST.get("button_number"))
+    state_change_value = int(request.POST.get("state_change_value"))
+    print(f"The value of button number is : {button_number}")
+    print(f"The value of state change value is : {state_change_value}")
+    state_changed = changePinStatus(button_number , state_change_value)
     if state_changed : 
         response["success"] = True
-        response["data"] = f"Changed : {switch} : {relay}"
+        response["data"] = f"Changed : {button_number} : {state_change_value}"
         return Response(
             response , 
         )
--- rasp.py
+++ rasp.py
@@ -1,9 +1,10 @@
-
 import time
 import requests
 import switches as sw
 import RPi.GPIO as gp
-
+import os
+token = os.environ.get("BIOS_TOKEN")
+print(token)
 gp.setmode(gp.BOARD)
 
 gp.setup(sw.SWITCH_1, gp.IN)	# switch 1
@@ -18,8 +19,11 @@ def toggleRelay(switch, relay):
     relay_state=gp.input(relay)
 #     gp.output(relay, int(not relay_state))
     print("off" if relay_state==1 else "on")
-    response = requests.get('http://localhost:8080/api/change-device-state/' ,
-        params = {"button_number" : int(relay) , "state_change_value": int(not relay_state)})
+    print(f"The value of relay is : {relay}")
+    print(f"The value of change state is : {int(not relay_state)}")
+    response = requests.post('http://localhost:8080/api/change-device-state/' ,
+        data = {"button_number" : int(relay) , "state_change_value": int(not relay_state)} ,
+        headers = {"Authorization": f"Bearer {token}" })
     print(response)
     
 # these last states will also be fetched from db
